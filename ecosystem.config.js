b451a792e2d79f2766bcc2bc6e5e437103d6a433
koofree <contact@koofree.com>
2025-03-12 10:17:39 +0900

    Update PM2 ecosystem configuration
    - Rename app name to 'firma-flip'
    - Reduce max memory restart from 1G to 256M

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: 'next-app',
+      name: 'firma-flip',
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: '256M',
       env: {
         NODE_ENV: 'development',
         PORT: 3001,

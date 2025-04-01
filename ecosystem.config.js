3dd0d3c252efd7bdb27d39ed76b0dfae850a5228
yongku.lee <yongku.lee@firmainnovation.com>
2025-04-01 12:54:31 +0900

    Update ecosystem configuration for development and production environments
    - Change development environment to use 'dev' argument for Next.js
    - Set production environment to use 'start' argument for Next.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,17 @@ module.exports = {
     {
       name: 'firma-flip',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '256M',
       env: {
+        args: 'dev',
         NODE_ENV: 'development',
         PORT: 3001,
       },
       env_production: {
+        args: 'start',
         NODE_ENV: 'production',
         PORT: 8080,
       },
